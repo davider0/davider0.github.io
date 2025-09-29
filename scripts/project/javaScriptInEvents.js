94d48be718533c53ee90d7075ff545fc41a3d579
davider <69058122+davider0@users.noreply.github.com>
2025-09-29 21:38:27 +0200

    feat: dialog system updated

--- scripts/project/javaScriptInEvents.js
+++ scripts/project/javaScriptInEvents.js
@@ -151,29 +151,25 @@ document.querySelector("body").style.backgroundColor = changeHue(""+rgbToHex(run
 
 	async Efights_Event5_Act1(runtime, localVars)
 	{
-		var texto = runtime.localVars.text;
-		
-		if(localVars.csvColumn != 0 && localVars.csvRow != 0){
-		    texto = C3.getFiles()[csvRow][csvColumn];
-		}
-		
-			console.log(runtime.localVars.eachLetterPause = runtime.localVars.eachLetterPause + runtime.localVars.pause);
+		var texto = runtime.globalVars.texto;
+			console.log(runtime.globalVars.eachLetterPause = runtime.globalVars.eachLetterPause + runtime.globalVars.pause);
 		if(texto[runtime.objects.gText.getFirstInstance().text.length + 1] == "^"){
 		
-		console.log(texto[runtime.objects.outputText.getFirstInstance().text.length + 1]);
+		console.log(texto[runtime.objects.gText.getFirstInstance().text.length + 1]);
 		/* si el siguiente caracter es un ^ se almacena el numero que viene después en "pause"*/
-			runtime.globalVars.pause = parseInt(runtime.objects.inputText.getFirstInstance().text[runtime.objects.outputText.getFirstInstance().text.length + 2],10);
+			runtime.globalVars.pause = parseInt(texto[runtime.objects.gText.getFirstInstance().text.length + 2],10);
 			
 			runtime.globalVars.eachLetterPause = runtime.globalVars.eachLetterPause + runtime.globalVars.pause;
 			
-			runtime.objects.inputText.getFirstInstance().text = texto.substring(0, texto.indexOf("^"+runtime.globalVars.pause)) + texto.substring(texto.indexOf("^"+runtime.globalVars.pause) + 2);
+			texto = texto.substring(0, texto.indexOf("^"+runtime.globalVars.pause)) + texto.substring(texto.indexOf("^"+runtime.globalVars.pause) + 2);
 			console.log(texto);
-		runtime.objects.outputText.getFirstInstance().text = texto.substring(0,runtime.objects.outputText.getFirstInstance().text.length + 1);
+		runtime.objects.gText.getFirstInstance().text = texto.substring(0,runtime.objects.gText.getFirstInstance().text.length + 1);
 		} else{ // y si no pues pause se queda en cero
 			runtime.globalVars.eachLetterPause = runtime.globalVars.eachLetterPause - runtime.globalVars.pause*2;
-			runtime.globalVars.pause = 0;
-			runtime.objects.outputText.getFirstInstance().text = texto.substring(0,runtime.objects.outputText.getFirstInstance().text.length + 1);
+		runtime.globalVars.pause = 0;
+			runtime.objects.gText.getFirstInstance().text = texto.substring(0,runtime.objects.gText.getFirstInstance().text.length + 1);
 		}
+		
 	}
 };
 
